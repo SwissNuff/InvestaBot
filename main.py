c703a3edf3ba135311f9a56ccb320c0a358cc332
Arian Rey <39580184+SwissNuff@users.noreply.github.com>
2021-10-31 09:19:14 +0000

    Add: Cryptocurrency Support

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ import requests
 import json
 from replit import db
 
-
 client = discord.Client()
 
 @client.event
 async def on_ready():
   print("We have logged in as (0.user)".format(client))
-
-
 
 # ===== BOT COMMANDS  =====  #
 def add_stonks(stock, buy_price):
@@ -28,6 +25,19 @@ def add_stonks(stock, buy_price):
   else:
     db["buyPrice"] = [buy_price]
 
+def add_crypto(stock, buy_price):
+  if "crypto_symbols" in db.keys():
+    crypto_symbols = db["crypto_symbols"]
+    crypto_symbols.append(stock)
+    db["crypto_symbols"] = crypto_symbols
+  else:
+    db["crypto_symbols"] = [stock]
+  if "crypto_buyPrice" in db.keys():
+    crypto_buyPrice = db["crypto_buyPrice"]
+    crypto_buyPrice.append(buy_price)
+    db["crypto_buyPrice"] = crypto_buyPrice
+  else:
+    db["crypto_buyPrice"] = [buy_price]
 
 def delete_stonks(index):
   symbols = db["symbols"]
@@ -38,6 +48,15 @@ def delete_stonks(index):
   db["symbols"] = symbols
   buyPrice = db["buyPrice"]
 
+def delete_crypto(index):
+  crypto_symbols = db["crypto_symbols"]
+  crypto_buyPrice = db["crypto_buyPrice"]
+  if len(crypto_symbols) > index:
+    del crypto_symbols[index]
+    del crypto_buyPrice[index]
+  db["crypto_symbols"] = crypto_symbols
+  crypto_buyPrice = db["crypto_buyPrice"]
+
 
 def get_stocks(smbl):
   response = requests.get(f"http://phisix-api4.appspot.com/stocks/{smbl}.json")
@@ -47,7 +66,11 @@ def get_stocks(smbl):
   amount = json_data['stock'][0]['price']['amount']
   return name, symbol, amount
 
-
+def get_crypto(smbl):
+  response = requests.get(f"https://api.binance.com/api/v3/avgPrice?symbol={smbl}USDT")
+  json_data = json.loads(response.text)
+  amount = json_data['price']
+  return amount
 
 @client.event
 async def on_message(message):
@@ -59,6 +82,8 @@ async def on_message(message):
   if msg.startswith("$stonks"):
     symbols = db["symbols"]
     buyPrice = db["buyPrice"]
+    crypto_symbols = db["crypto_symbols"]
+    crypto_buyPrice = db["crypto_buyPrice"]
     for i in range(len(symbols)):
       output = get_stocks(symbols[i])
       gains = ((float(output[2]) - float(buyPrice[i])) / float(buyPrice[i]))*100
@@ -71,12 +96,31 @@ Buy price: PHP {buyPrice[i]}
 gains/loss: {round(gains, 2)}%
 ```
         """
+      )
+    for i in range(len(crypto_symbols)):
+      current_price = get_crypto(crypto_symbols[i])
+      gains = ((float(current_price) - float(crypto_buyPrice[i])) / float(crypto_buyPrice[i]))*100
+      await message.channel.send(
+        f"""
+        Stock: {crypto_symbols[i]}
+```py
+Current price: USD {current_price}
+Buy price: USD {crypto_buyPrice[i]}
+gains/loss: {round(gains, 2)}%
+```
+        """
         )
   
   if msg.startswith("$add"):
     stock = msg.split("$add ",1)[1]
     smbl, buy_price = stock.split()
     add_stonks(smbl, buy_price)
+    await message.channel.send(f"{smbl} has been added to lists with a buy price of {buy_price}.")
+
+  if msg.startswith("$cadd"):
+    stock = msg.split("$cadd ",1)[1]
+    smbl, buy_price = stock.split()
+    add_crypto(smbl, buy_price)
     await message.channel.send(f"{smbl} has been added to lists with a buy price of {buy_price}.")
 
   if msg.startswith("$del"):
@@ -87,12 +131,23 @@ gains/loss: {round(gains, 2)}%
       symbols = db["symbols"]
     await message.channel.send(symbols)
 
+  if msg.startswith("$cdel"):
+    crypto_symbols = []
+    if "crypto_symbols" in db.keys():
+      index = int(msg.split("$cdel",1)[1])
+      delete_crypto(index)
+      crypto_symbols = db["crypto_symbols"]
+    await message.channel.send(crypto_symbols)
+
   if msg.startswith("$show"):
     await message.channel.send("Current stocks bought")
     symbols = []
     if "symbols" in db.keys():
       symbols = db["symbols"]
+    if "crypto_symbols" in db.keys():
+      crypto_symbols = db["crypto_symbols"]
     await message.channel.send(symbols)
+    await message.channel.send(crypto_symbols)
 
   if msg.startswith("$help"):
     await message.channel.send("`$help` : shows list of commands\n`$add <stock symbol> <buyprice>` : add stock symbol to the list\n `$del <list index>`: deletes stock symbol to the list\n`$show:`:Show all stocks bought")
